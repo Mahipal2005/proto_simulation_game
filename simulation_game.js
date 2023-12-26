f2565d754876a35f699a8340cc6ca20fe3dd01a8
Mahipal <139787677+Mahipal2005@users.noreply.github.com>
2023-12-27 03:20:03 +0530

    Update simulation_game.js

--- simulation_game.js
+++ simulation_game.js
@@ -1,40 +1,48 @@
-function Vehicle(brand,model,speed,fuelType){
-    this.brand=brand;
-    this.model=model;
-    this.speed=speed;
-    this.fuelType=fuelType
+function Vehicle(brand, model, speed, fuelType) {
+    this.brand = brand;
+    this.model = model;
+    this.speed = speed;
+    this.fuelType = fuelType;
 }
 
-Vehicle.prototype.accelerate=function(s){
-   
-            console.log(s=s+1)
+Vehicle.prototype.accelerate = function () {
+    this.speed++;
+};
 
-}
-Vehicle.prototype.brake=function(s){
-    console.log(s=s-1)
-}
-Vehicle.prototype.refuel=function(){
-    console.log(" the vehicle is full refueling.")
-}
+Vehicle.prototype.brake = function () {
+    this.speed--;
+};
 
+Vehicle.prototype.refuel = function () {
+    console.log("The vehicle is fully refueled.");
+};
 
-function Car(brand, model, speed, fuelType, numberOfWheels){
+function Car(brand, model, speed, fuelType, numberOfWheels) {
     Vehicle.call(this, brand, model, speed, fuelType);
-        this.numberOfWheels = numberOfWheels;
+    this.numberOfWheels = numberOfWheels;
 }
-Car.prototype.honk=function(){
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+Car.prototype.honk = function () {
     console.log("Honk! Honk!");
+};
+
+function Aeroplane(brand, model, speed, fuelType, numberOfEngines, landingGear) {
+    Vehicle.call(this, brand, model, speed, fuelType);
+    this.numberOfEngines = numberOfEngines;
+    this.landingGear = landingGear;
 }
 
-function Aeroplain(brand, model, speed, fuelType,numberOfEngines,LandingGear){
-    Vehicle.call(this, brand, model, speed, fuelType)
-    this.numberOfEngines=numberOfEngines;
-    this.LandingGear=LandingGear;
-}
-Aeroplain.prototype.takeoff=function(){
-    console.log("airplane is taking off")
-}
-let mycar=new Car("Tata","Nexon",50,"petrol",4)
+Aeroplane.prototype = Object.create(Vehicle.prototype);
+Aeroplane.prototype.constructor = Aeroplane;
 
-let myaeroplane=new Aeroplain("india","air","500","disel",2,5)
-console.log(myaeroplane)
+Aeroplane.prototype.takeoff = function () {
+    console.log("Airplane is taking off");
+};
+
+let mycar = new Car("Tata", "Nexon", 50, "petrol", 4);
+
+let myaeroplane = new Aeroplane("India", "Air", 500, "diesel", 2, 5);
+console.log(myaeroplane);
